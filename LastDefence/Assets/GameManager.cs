5f1253c3270befd1d309fb6ae240f18845fc9dae
Winston Leung <wleung85@uwo.ca>
2018-03-10 18:39:09 -0500

    Added GameManager to MenuScreen. Details on how to access elements inside script are given although we may just want to make it static instead

---
+++ LastDefence/Assets/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+	public int score;
+
+	void Awake () {
+		if (instance == null) 
+			instance = this;
+		else if (instance != null)
+			Destroy(gameObject);
+		DontDestroyOnLoad(transform.gameObject);
+	}
+
+	/********************************************
+	To access GameManager from another script
+
+	GameObject GM;								//Variable holds GameManager object
+	GM = GameObject.Find("GameManager");		//GameObject.Find to get GameManager
+	GMScript = GM.GetComponent<GameManager>();	//GetComponent to access GameManager script inside object
+
+	Debug.Log(GM.score);						//int score can now be accessed and used
+
+
+	*********************************************/
+}
