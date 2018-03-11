9f7c245421ed16ead0100fcfc1ff58e9fc4f40be
Winston Leung <wleung85@uwo.ca>
2018-03-11 01:43:59 -0500

    Added player controls for the tank and colliders for the floor and walls

---
+++ LastDefence/Assets/Controller_2D.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller_2D : MonoBehaviour {
+
+	private Rigidbody2D myRigidBody;
+	[SerializeField]
+	public float movementSpeed = 6.5f;
+
+	// Use this for initialization
+	void Start () {
+		myRigidBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float horizontal = Input.GetAxis ("Horizontal");
+		HandleMovement (horizontal);
+	}
+
+	private void HandleMovement(float horizontal){
+		myRigidBody.velocity = new Vector2 (horizontal * movementSpeed, myRigidBody.velocity.y);
+	}
+}
