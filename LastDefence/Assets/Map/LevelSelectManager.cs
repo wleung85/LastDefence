e12b6157a0be419f115c0018c4da2c4283473e31
Haitian Yu <hyu222@uwo.ca>
2018-03-12 22:52:46 -0400

    Started work on map
    -added map images
    - added level labels
    - added locks for levels that cannot be accessed yet

---
+++ LastDefence/Assets/Map/LevelSelectManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectManager : MonoBehaviour {
+	
+	public string[] levelTags;
+	
+	public GameObject[] locks;
+	
+	public bool[] levelUnlocked;
+	
+	PlayerPrefs.SetInt(level1Tag, 1);
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < levelTags.Length; i++){
+			if(PlayerPrefs.GetInt(levelTags[i]) == null){
+				levelUnlocked[i] = false;
+			}
+			else if(PlayerPrefs.GetInt(levelTags[i]) == 0) {
+				levelUnlocked[i] = false;
+			}
+			else{
+				levelUnlocked[i] = true;
+		}
+		
+		if(levelUnlocked[i]){
+			locks[i].SetActive(false);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
