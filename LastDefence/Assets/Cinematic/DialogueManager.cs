37c2ef11e0d7212f7e7af824bb17a1710027bad2
Winston Leung <wleung85@uwo.ca>
2018-03-14 23:15:22 -0400

    Added assets for cinematic and started dialogue manager scripts

---
+++ LastDefence/Assets/Cinematic/DialogueManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour {
+
+	private Queue<string> sentences;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+}
