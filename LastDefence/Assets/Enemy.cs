bd4102c86d9032cb48f2c7b4ec811fb547f394ba
Ryan Dabir <rdabir@uwo.ca>
2018-03-16 16:02:40 -0400

    enemy able to move across screen

---
+++ LastDefence/Assets/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 5f;
+    GameObject ebomb;
+
+    // Use this for initialization
+    void Start()
+    {
+        ebomb = Resources.Load("Battle/bomb1") as GameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+        GameObject bomb = Instantiate(ebomb) as GameObject;
+        bomb.transform.Translate(Vector3.down * speed * Time.deltaTime);
+	}
+}
