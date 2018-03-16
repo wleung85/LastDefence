6f69e627b4aca62fa9d994e4cc10edd99bb73cc5
Haitian Yu <hyu222@uwo.ca>
2018-03-16 16:46:42 -0400

    added bullets and basic bullet movement
    still need to implement the spawner to follow the tank arm

---
+++ LastDefence/Assets/Bullet_Destroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Destroy : MonoBehaviour {
+
+	public Transform bullet;
+	public float collisionRadius = 0f;
+	public bool collided = false;
+	public LayerMask whatToCollideWith;
+	
+	void FixedUpdate(){
+		collided = Physics2D.OverlapCircle(bullet.position,collisionRadius, whatToCollideWith);
+		if(collided)
+			Destroy(gameObject);
+		if(!GetComponent<Renderer>().isVisible)
+			Destroy(gameObject);
+	}
+}
---
+++ LastDefence/Assets/Bullet_Spawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Spawn : MonoBehaviour {
+	
+	public GameObject bullet;
+	public Transform spawnPoint;
+	
+	void fixedUpdate(){
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool shoot = Input.GetMouseButtonDown(0);
+		
+		if(shoot){
+			Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
+		}
+	}
+}
