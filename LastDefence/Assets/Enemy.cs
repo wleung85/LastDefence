a578f5b119a244f79db79df0d8b726b23f5f7253
Ryan Dabir <rdabir@uwo.ca>
2018-03-16 16:25:14 -0400

    removed useless code and made enemy despawn after it leaves the screen

--- LastDefence/Assets/Enemy.cs
+++ LastDefence/Assets/Enemy.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
 
     public float speed = 5f;
-    GameObject ebomb;
 
     // Use this for initialization
     void Start()
     {
-        ebomb = Resources.Load("Battle/bomb1") as GameObject;
     }
 	
 	// Update is called once per frame
 	void Update () 
     {
         transform.Translate(Vector3.left * speed * Time.deltaTime);
-
-        GameObject bomb = Instantiate(ebomb) as GameObject;
-        bomb.transform.Translate(Vector3.down * speed * Time.deltaTime);
+        Destroy();
 	}
+
+    void Destroy ()
+    {
+        Destroy(gameObject, 5);
+    }
 }
