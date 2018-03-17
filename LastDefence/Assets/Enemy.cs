8bd68d80518d0917d341f24006ba03f262f7c239
Ryan Dabir <rdabir@uwo.ca>
2018-03-16 20:02:06 -0400

    plane can now drop bomb on button press, need to change to random however

--- LastDefence/Assets/Enemy.cs
+++ LastDefence/Assets/Enemy.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour {
 
     public float speed = 5f;
+    public float bombSpeed = 5f;
+    public GameObject bomb;
+    public Transform spawnPoint;
+    public GameObject fallingBomb;
 
     // Use this for initialization
     void Start()
     {
+        bomb = GameObject.Find("Bomb");
+        spawnPoint = GameObject.Find("Plane").transform;
     }
 	
 	// Update is called once per frame
@@ -16,6 +24,12 @@ public class Enemy : MonoBehaviour {
     {
         transform.Translate(Vector3.left * speed * Time.deltaTime);
         Destroy();
+        bool shoot = Input.GetKeyDown(KeyCode.A);
+        if (shoot)
+        {
+            fallingBomb = Instantiate(bomb, spawnPoint.position, spawnPoint.rotation);
+        }
+        fallingBomb.transform.Translate(Vector3.down * bombSpeed * Time.deltaTime);
 	}
 
     void Destroy ()
