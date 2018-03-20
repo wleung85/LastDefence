52698136571e09449109fb78197654bb990b8770
Winston Leung <wleung85@uwo.ca>
2018-03-20 19:52:17 -0400

    Finished cinematics

--- LastDefence/Assets/Cinematic/DialogueManager.cs
+++ LastDefence/Assets/Cinematic/DialogueManager.cs
@@ -3,25 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
+// Character/images in the hierarchy has to be named according to their Name in dialogue
+// To create dialogue, copy template and put lines in allDialogues
 public class DialogueManager : MonoBehaviour {
 
 	public TextMeshProUGUI nameText;
 	public TextMeshProUGUI dialogueText;
+	public Text continueButton;
+	public GameObject allDialogues;
+	private GameObject currCharacter;
+
+	private allDialogue allDialoguesScript;
+	public Queue<Dialogue> characterlines;
 	private Queue<string> sentences;
+	
+	private bool cutsceneDone = false;
 	private bool textRunning;
 	private string tempSentence;
 
-	public Animator animator;
+	public Animator dialogueBoxAnimator;
+	public Animator char1Animator;
+	public Animator char2Animator;
+	private Animator currCharAnimator;
 	
 	// Use this for initialization
 	void Start () {
+		// Grab all dialogues and queue them into characterlines
+		allDialoguesScript = allDialogues.GetComponent<allDialogue>();
 		sentences = new Queue<string>();
+		characterlines = new Queue<Dialogue>();
+		foreach (Dialogue dialogue in allDialoguesScript.dialogues) {
+			characterlines.Enqueue(dialogue);
+		}
+		StartCoroutine(DelayStart());
 	}
+
+	private IEnumerator DelayStart() {
+		yield return new WaitForSeconds(1);	// Waits one second
+		StartDialogue(characterlines.Dequeue());
+	}
+
 	public void StartDialogue (Dialogue dialogue) {
-		animator.SetBool("IsOpen",true);
-		
+		dialogueBoxAnimator.SetBool("IsOpen",true);
+		char1Animator.SetBool("CharacterFadeIn",true);
+		char2Animator.SetBool("CharacterFadeIn",true);
+		StartNextDialogue(dialogue);
+	}
+
+	public void StartNextDialogue (Dialogue dialogue) {
 		nameText.text = dialogue.name;
+		currCharacter = GameObject.Find(dialogue.name);
+		currCharAnimator = currCharacter.GetComponent<Animator>();
+
 		sentences.Clear();
 
 		foreach(string sentence in dialogue.sentences) {
@@ -35,18 +70,30 @@ public class DialogueManager : MonoBehaviour {
 	public void DisplayNextSentence() {
 		if (!textRunning) {
 			if (sentences.Count == 0) {
-				EndDialogue();
+				if (characterlines.Count == 0) {
+					EndDialogue();
+				}
+				else {
+					StartNextDialogue(characterlines.Dequeue());
+				}
 				return;
+				
 			}
 			string sentence = sentences.Dequeue();
 			tempSentence = sentence;
-			// StopAllCoroutines();
 			textRunning = true;
-			StartCoroutine(TypeSentence(sentence));
+			currCharAnimator.SetBool("CharacterTalking",true);
+			if (sentences.Count == 0 && characterlines.Count == 0) {
+				StartCoroutine(TypeSentence(sentence, true));
+			}
+			else {
+				StartCoroutine(TypeSentence(sentence, false));
+			}
 		}
 		else {
 			StopAllCoroutines();
 			EndSentence(tempSentence);
+			currCharAnimator.SetBool("CharacterTalking",false);
 			textRunning = false;
 		}
 		
@@ -56,17 +103,28 @@ public class DialogueManager : MonoBehaviour {
 		dialogueText.text = sentence;
 	}
 
-	IEnumerator TypeSentence (string sentence) {
+	IEnumerator TypeSentence (string sentence, bool last) {
 		textRunning = true;
 		dialogueText.text = "";
 		foreach (char letter in sentence.ToCharArray()) {
 			dialogueText.text += letter;
 			yield return null;
 		}
+		currCharAnimator.SetBool("CharacterTalking",false);
 		textRunning = false;
+		if(last){
+			EndDialogue();
+		}
 	}
 
 	private void EndDialogue() {
-		Debug.Log("End of conversation.");
+		currCharAnimator.SetBool("CharacterTalking",false);
+		continueButton.text = "EXIT >>";
+		cutsceneDone = true;
+	}
+
+	public void changeScene() {
+		if (cutsceneDone)
+			SceneManager.LoadScene("LevelMap");
 	}
 }
--- LastDefence/Assets/Cinematic/DialogueTrigger.cs
+++ LastDefence/Assets/Cinematic/DialogueTrigger.cs
@@ -8,5 +8,6 @@ public class DialogueTrigger : MonoBehaviour {
 
 	public void TriggerDialogue() {
 		FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+		Debug.Log("Trigger activated");
 	}
 }
