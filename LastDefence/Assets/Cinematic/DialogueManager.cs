ec03d51a3fc58273722625d14c1b4b1dab01b329
Winston Leung <wleung85@uwo.ca>
2018-03-20 17:01:46 -0400

    Primitive dialogue system in place. Now need to implement changing dialogues

--- LastDefence/Assets/Cinematic/DialogueManager.cs
+++ LastDefence/Assets/Cinematic/DialogueManager.cs
@@ -1,14 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour {
 
+	public TextMeshProUGUI nameText;
+	public TextMeshProUGUI dialogueText;
 	private Queue<string> sentences;
+	private bool textRunning;
+	private string tempSentence;
+
+	public Animator animator;
 	
 	// Use this for initialization
 	void Start () {
+		sentences = new Queue<string>();
+	}
+	public void StartDialogue (Dialogue dialogue) {
+		animator.SetBool("IsOpen",true);
 		
+		nameText.text = dialogue.name;
+		sentences.Clear();
+
+		foreach(string sentence in dialogue.sentences) {
+			sentences.Enqueue(sentence);
+		}
+
+		textRunning = false;
+		DisplayNextSentence();
 	}
 	
+	public void DisplayNextSentence() {
+		if (!textRunning) {
+			if (sentences.Count == 0) {
+				EndDialogue();
+				return;
+			}
+			string sentence = sentences.Dequeue();
+			tempSentence = sentence;
+			// StopAllCoroutines();
+			textRunning = true;
+			StartCoroutine(TypeSentence(sentence));
+		}
+		else {
+			StopAllCoroutines();
+			EndSentence(tempSentence);
+			textRunning = false;
+		}
+		
+	}
+
+	public void EndSentence(string sentence) {
+		dialogueText.text = sentence;
+	}
+
+	IEnumerator TypeSentence (string sentence) {
+		textRunning = true;
+		dialogueText.text = "";
+		foreach (char letter in sentence.ToCharArray()) {
+			dialogueText.text += letter;
+			yield return null;
+		}
+		textRunning = false;
+	}
+
+	private void EndDialogue() {
+		Debug.Log("End of conversation.");
+	}
 }
