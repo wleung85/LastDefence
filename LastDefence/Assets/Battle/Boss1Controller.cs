bee9aed5bd5909106a119310466206c1bcdd187f
Winston Leung <wleung85@uwo.ca>
2018-03-21 23:27:05 -0400

    Fixed crosshair being slightly off from cursor position

---
+++ LastDefence/Assets/Battle/Boss1Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss1Controller : MonoBehaviour {
+
+    public int health;
+
+	// Use this for initialization
+	void Start () {
+        health = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D (Collision2D collisionInfo)
+    {
+        Debug.Log("Collision detected");
+        if (collisionInfo.collider.tag.Equals("PlayerProjectile"))
+        {
+            DecreaseHealth();
+            Debug.Log("Position of projectile hit:" + collisionInfo.transform.position);
+        }
+    }
+
+    private void DecreaseHealth()
+    {
+        Debug.Log("Decreasing Boss1 Health");
+    }
+}
