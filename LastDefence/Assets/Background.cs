b0f0465f99a1b74814c4906d7d0afcc705e29768
Anthony Giugno <agiugno2@uwo.ca>
2018-03-23 18:01:06 -0400

    First implementation.

---
+++ LastDefence/Assets/Background.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+	public float speed;
+	private Renderer renderer; 
+	private float offset;
+
+	private void Start() { 
+		renderer = GetComponent<Renderer>(); 
+	} 
+
+	private void Update() { 
+		//renderer.material.mainTextureOffset = new Vector2(Time.time * speed, 0); 
+		float horizontal = Input.GetAxis ("Horizontal");
+		offset = offset + (horizontal * speed);
+		renderer.material.mainTextureOffset = new Vector2(offset, 0);
+	}﻿
+}
