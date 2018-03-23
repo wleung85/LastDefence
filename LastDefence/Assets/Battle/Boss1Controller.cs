3aba41a7a98482ecaded3a8e8968e5ba86e53a89
Winston Leung <wleung85@uwo.ca>
2018-03-23 00:23:55 -0400

    Added Boss death animation and start animation and health bar

--- LastDefence/Assets/Battle/Boss1Controller.cs
+++ LastDefence/Assets/Battle/Boss1Controller.cs
@@ -4,19 +4,58 @@ using UnityEngine;
 
 public class Boss1Controller : MonoBehaviour {
 
-    public int health;
+    // Health
+    private int health;
+    public int maxHealth;
+
+    // Movement
+    [SerializeField]
+    Transform[] waypoints;
+    [SerializeField]
+    float moveSpeed = 2f;
+    int waypointIndex;
+    private bool forward;
+    int defeatedMoveFrames = 0;
+    Vector2 defeatedFinalPos;
+
+    private bool defeated = false;
+    private bool intro = true;
+
+    // Prefabs
     public GameObject explosionPrefab;      // Drag and drop the prefabs into here through the Unity inspector
     public GameObject explosionPrefab2;
     private GameObject explosionPrefabClone;
 
+    // Gameobjects
+    GameObject bossHealth;
+
 	// Use this for initialization
 	void Start () {
-        health = 100;
-	}
+        transform.position = new Vector3(0f, 7.11f, 0f);
+        forward = true;
+        health = maxHealth;
+
+        //disable hit box, boss health, and firing until after intro
+        GetComponent<PolygonCollider2D>().enabled = false;
+        bossHealth = GameObject.Find("BossHealth");
+        bossHealth.SetActive(false);
+        GameObject.Find("Tank").GetComponent<Bullet_Spawn>().enabled = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (!defeated && !intro)
+        {
+            Move();
+        }
+        else if (intro)
+        {
+            IntroMove();
+        }
+        else if (defeated)
+        {
+            DefeatedMove();
+        }
 	}
 
     void OnCollisionEnter2D (Collision2D collisionInfo)
@@ -33,12 +72,84 @@ public class Boss1Controller : MonoBehaviour {
             {
                 explosionPrefabClone = Instantiate(explosionPrefab2, collisionInfo.transform.position, Quaternion.identity);
             }
-            Destroy(explosionPrefabClone, 1.2f); //time to kill explosion is currently hardcoded in
+            Destroy(explosionPrefabClone, 1.7f); //time to kill explosion is currently hardcoded in
         }
     }
 
     private void DecreaseHealth()
     {
-        Debug.Log("DecreaseHealth() function called");
+        health--;
+        float healthPerc = (float)health / maxHealth;
+        GameObject.Find("BossHealthCurrent").transform.localScale = new Vector3(healthPerc, 1, 1);
+        if (health == 0)
+        {
+            Defeated();
+        }
+    }
+
+    private void Move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,
+            waypoints[waypointIndex].transform.position, 
+            moveSpeed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, waypoints[waypointIndex].transform.position) <= 0.1) {
+            waypointIndex = forward ? waypointIndex + 1 : waypointIndex - 1;
+        }
+
+        if (waypointIndex == waypoints.Length - 1)
+        {
+            forward = false;
+        }
+        else if (waypointIndex == 0)
+        {
+            forward = true;
+        }
+    }
+
+    private void Defeated()
+    {
+        GetComponent<PolygonCollider2D>().enabled = false;
+        defeated = true;
+        defeatedFinalPos = new Vector2(transform.position.x, -3.77f);
+    }
+
+    private void DefeatedMove()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,
+            defeatedFinalPos,
+            moveSpeed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, defeatedFinalPos) > 0.1)
+        {
+            // Execute every 3 frames moving down
+            defeatedMoveFrames++;
+            if (defeatedMoveFrames >= 5)
+            {
+                defeatedMoveFrames = 0;
+                float randX = Random.Range(-2.7f, 2.7f);
+                float randY = Random.Range(-1f, 1);
+                explosionPrefabClone = Instantiate(explosionPrefab, 
+                    new Vector2(transform.position.x + randX, transform.position.y + randY),
+                    Quaternion.identity);
+                Destroy(explosionPrefabClone, 1.7f);
+            }
+        }
+    }
+
+    private void IntroMove()
+    {
+        // Move to starting position
+        transform.position = Vector2.MoveTowards(transform.position,
+            new Vector2(transform.position.x, 2.12f),
+            moveSpeed * Time.deltaTime);
+
+        // If made it to position
+        if (Vector2.Distance(transform.position, new Vector2(transform.position.x, 2.12f)) <= 0.1)
+        {
+            intro = false;
+            GetComponent<PolygonCollider2D>().enabled = true;
+            bossHealth.SetActive(true);
+            GameObject.Find("Tank").GetComponent<Bullet_Spawn>().enabled = true;
+        }
     }
 }
